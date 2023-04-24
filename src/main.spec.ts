a0dc3d82bfa5c51e82879065e9a62a6064462fd6
Natan Souza <partisansouza@gmail.com>
2023-04-24 19:03:44 +0100

    feat: initial setup with unit testing

---
+++ src/main.spec.ts
@@ -0,0 +1,74 @@
+import { anagram, beeramid, palyndrome, pyramid, alternateSort } from '@/main';
+
+describe('Anagram', () => {
+  it('should return true', () => {
+    expect(anagram('study', 'dusty')).toStrictEqual(true);
+  });
+
+  it('should return false because of length', () => {
+    expect(anagram('example', 'false')).toStrictEqual(false);
+  });
+
+  it('should return false because of letters', () => {
+    expect(anagram('example', 'bananas')).toStrictEqual(false);
+  });
+
+  it('fixed test', () => {
+    expect(anagram('ABCD', 'DABC')).toStrictEqual(true);
+    expect(anagram('AABB', 'BBAA')).toStrictEqual(true);
+    expect(anagram('AAAB', 'AAB')).toStrictEqual(false);
+    expect(anagram('AAAB', 'AABB')).toStrictEqual(false);
+    expect(anagram('AAAB', 'AABC')).toStrictEqual(false);
+    expect(anagram('ACBA', 'ABBC')).toStrictEqual(false);
+    expect(anagram('A', 'B')).toStrictEqual(false);
+  });
+});
+
+describe('Palyndrome', () => {
+  it('should return true', () => {
+    expect(palyndrome('natan')).toStrictEqual(true);
+  });
+
+  it('should return false', () => {
+    expect(palyndrome('example')).toStrictEqual(false);
+  });
+});
+
+describe('Pyramid', () => {
+  it('should return a pyramid', () => {
+    expect(pyramid(3)).toStrictEqual('\n  #  \n ### \n#####\n');
+  });
+});
+
+describe('Beeramid', () => {
+  it('Fixed tests', () => {
+    expect(beeramid(9, 2)).toStrictEqual(1);
+    expect(beeramid(10, 2)).toStrictEqual(2);
+    expect(beeramid(11, 2)).toStrictEqual(2);
+    expect(beeramid(21, 1.5)).toStrictEqual(3);
+    expect(beeramid(454, 5)).toStrictEqual(5);
+    expect(beeramid(455, 5)).toStrictEqual(6);
+    expect(beeramid(4, 4)).toStrictEqual(1);
+    expect(beeramid(3, 4)).toStrictEqual(0);
+    expect(beeramid(0, 4)).toStrictEqual(0);
+    expect(beeramid(-1, 4)).toStrictEqual(0);
+    expect(beeramid(-70, 3.7)).toStrictEqual(0);
+  });
+});
+
+describe('Alternate sort', () => {
+  it('Should be different', () => {
+    const randoms = Array.from({ length: Math.random() * 10 }, () =>
+      Array.from({ length: Math.random() * 10 }, () =>
+        Math.floor(Math.random() * 10),
+      ),
+    );
+
+    for (const array of randoms) {
+      const returned = alternateSort(array);
+
+      expect(returned.length).toEqual(array.length);
+      expect(returned).toBeInstanceOf(Array);
+    }
+  });
+});
---
+++ src/main.ts
@@ -0,0 +1,114 @@
+// Complexity: O(n log n)
+const anagram = (str1: string, str2: string): boolean => {
+  // Not the same length, not an anagram!
+  if (str1.length !== str2.length) return false;
+
+  // Sort the strings and compare them
+  (str1 = str1.split('').sort().join('')),
+    (str2 = str2.split('').sort().join(''));
+  if (str1 !== str2) return false;
+
+  // If we get here, they're anagrams
+  return true;
+};
+
+// Complexity: O(n)
+const palyndrome = (str: string): boolean => {
+  // Reverse string without split/join
+  let reverse = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+
+  // Compare the string to its reverse
+  if (str === reverse) return true;
+
+  // If we get here, it's not a palyndrome
+  return false;
+};
+
+// Complexity: O(n)
+const pyramid = (size: number): string => {
+  let i = 1,
+    finalString = '\n';
+
+  do {
+    // Spacing (size - quantity of ascii).
+    const str = ' '.repeat(size - i);
+    // Quantity of ascii chars (always a odd and - 1 to begin with just one).
+    const str2 = '#'.repeat(i * 2 - 1);
+    // Append to our initial string to create an final result.
+    finalString += `${str}${str2}${str}\n`;
+    // Sum up our indicator.
+    i++;
+  } while (size >= i);
+
+  return finalString;
+};
+
+// Complexity: O(n)
+const beeramid = (bonus: number, price: number): number => {
+  // Floor the total of cans we can buy using bitwise.
+  const totalCans = ~~(bonus / price);
+
+  let level = 0,
+    beer = 0;
+
+  while (beer <= totalCans) {
+    level++;
+    // The beer cans are the square of the level.
+    beer += level ** 2;
+  }
+
+  // Level - 1 because we need to return the last level.
+  const final = level - 1;
+
+  // If we can't buy any beer, return 0.
+  if (final < 0) return 0;
+
+  return final;
+};
+
+// Alternating sort
+// Given an array of integers e, arrange the elements in it so that:
+
+// e[0] ≤ e[1] ≥ e[2] ≤ e[3] ≥ e[4] ...
+
+// The elements in e need not be unique (they may be repeated)
+
+// For example:
+// 5 2 1 7 9 8 ->  1 ≤ 7 ≥ 5 ≤ 9 ≥ 2 ≤ 8  or  2 ≤ 5 ≥ 1 ≤ 9 ≥ 7 ≤ 8 or ...
+// 1 2 3 4 5 6 ->  1 ≤ 3 ≥ 2 ≤ 5 ≥ 4 ≤ 6  or ...
+// -2 3 3 -3   ->  3 ≤ 3 ≥ -3 ≤ -2  or  -2 ≤ 3 ≥ -3 ≤ 3 or ...
+
+// Complexity: O(n)
+// Space: O(1)
+const alternateSort = (arr: number[]): number[] => {
+  // Swap variable
+  let swap = 0;
+
+  // Iterate over the array
+  for (let i = 0; i < arr.length; i++) {
+    // If we are in the last element, break the loop.
+    if (!arr[i + 1]) break;
+
+    // If the index is odd, we need to check if the next element is greater than the current.
+    if (i % 2 != 0) {
+      if (arr[i] <= arr[i + 1]) {
+        continue;
+      }
+    } else {
+      // Otherwise we need to check if the next element is less than the current.
+      if (arr[i] >= arr[i + 1]) {
+        // If it is, swap the elements.
+        swap = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = swap;
+      }
+    }
+  }
+
+  return arr;
+};
+
+export { anagram, palyndrome, pyramid, beeramid, alternateSort };
